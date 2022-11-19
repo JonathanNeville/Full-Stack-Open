87b99f360411ada6041a398d77ecdba17170ac48
CarolinaReefer <89300183+CarolinaReefer@users.noreply.github.com>
2022-11-19 11:56:33 +0100

    completed exercises through 4.14

--- part4/blog_list/__tests__/node_api.test.js
+++ part4/blog_list/__tests__/node_api.test.js
@@ -40,6 +40,89 @@ test('verify that unique identifier is named id', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
+test('making an HTTP POST request to /api/blogs creates a new blog post', async () => {
+    const newBlog = {
+        "title": "test",
+        "author": "testson",
+        "url": "blog.com/test",
+        "likes": 21
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+})
+
+test('if making a post request with likes propery missing defaults to 0', async () => {
+    const newBlog = {
+        "title": "test",
+        "author": "testson",
+        "url": "blog.com/test"
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[2].likes).toEqual(0)
+})
+
+test('trying to post a blog with no title or url response status is 400', async () => {
+    const newBlog = {
+        "author": "testson",
+        "likes": "1",
+        
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        
+})
+
+test('deleting blog deletes blog', async () => {
+    const blogsAtstart = await (await api.get('/api/blogs')).body
+    const blogToDelete = blogsAtstart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+    
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(blogsAtstart.length - 1)
+})
+
+test('updating a blogpost', async () => {
+
+    const blogsAtstart = await (await api.get('/api/blogs')).body
+    const blogToUpdate = blogsAtstart[0]
+    
+    const updatedBlog = {
+        "title": "update",
+        "author": "updateson",
+        "url": "blog.com/updated",
+        "likes": 123
+    }
+
+    await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog)
+        .expect(204)
+
+    const response = await api.get(`/api/blogs/${blogToUpdate.id}`)
+    expect(response.body.likes).toEqual(updatedBlog.likes)
+}, 100000)
+
 afterAll(() => {
     mongoose.connection.close()
   })
--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const { request, response } = require('../app')
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response) => {
@@ -7,10 +8,65 @@ blogRouter.get('/', async (request, response) => {
   })
   
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-    
+    const body = request.body
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0
+    })
+
+    if (blog.title && blog.url) {
+      const savedBlog = await blog.save()
+      response.status(201).json(savedBlog)
+    }
+    else {
+      response.status(400).end()
+    } 
+})
+
+blogRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    }
+    else{
+      response.status(404).end
+    }
+  }
+  catch(exception) {
+    response.status(400)
+  }
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  try  {
+    await Blog.findByIdAndRemove(id)
+    response.status(204).end()
+  }
+  catch (exception) {
+    response.status(404).end()
+  }
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  try  {
+    await Blog.findByIdAndUpdate(id, blog)
+    response.status(204).end()
+  }
+  catch (exception) {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogRouter
