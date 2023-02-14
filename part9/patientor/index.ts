b0e70f9d19bca25f2d57ec6b930164d84c82ba89
grynpipa <jonathanneville75@gmail.com>
2023-02-14 21:08:52 +0100

    finished exercise 9.9

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
 
-const PORT = 3004;
+app.use(cors())
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
+    
 });
 
 app.listen(PORT, () => {
