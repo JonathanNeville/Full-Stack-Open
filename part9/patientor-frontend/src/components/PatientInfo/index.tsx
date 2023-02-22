59e2b3ec65f263f96304d74c9ad80f13e6548c85
grynpipa <jonathanneville75@gmail.com>
2023-02-22 09:28:47 +0100

    finished exercise 9.21

---
+++ part9/patientor-frontend/src/components/PatientInfo/index.tsx
@@ -0,0 +1,34 @@
+import { Female, Male, Transgender } from "@mui/icons-material"
+import { Typography } from "@mui/material"
+import { useParams } from "react-router-dom"
+import { Patient } from "../../types"
+import patientService from "../../services/patients"
+import { useEffect, useState } from "react"
+
+
+
+const PatientInfo = () => {
+    const [patient, setPatient] = useState<Patient | null>(null)
+    const id = useParams().id 
+    
+    useEffect(() => {
+         patientService.getPatientById(id!).then((p) => setPatient(p.data))
+        }, [id]
+    )
+
+    if (!patient) {
+        return (
+            <p>Error</p>
+        )
+    }
+
+    return(
+        <div>
+            <Typography variant="h4">{patient.name} {patient.gender === "female" ? <Female/> : patient.gender === "male" ? <Male/>: <Transgender/>}</Typography>
+            <Typography>ssh: {patient.ssn}</Typography>
+            <Typography>occupation: {patient.occupation}</Typography>
+        </div>
+    )
+}
+
+export default PatientInfo
